2426a9f930a48261823f7c6f430697ba4d9f0a4c
mtctr <mateustargino95@gmail.com>
2017-05-25 11:24:28 -0300

    DONE: 1.a,c,d

--- Atividade 2/serverTCP.py
+++ Atividade 2/serverTCP.py
@@ -1,8 +1,17 @@
-import socket
+import socket, select
+
+#classe para o usuario
+class User:
+
+    def __init__(self, socket):
+        self.socket = socket
+        self.nickname = None
+    #troca nome dos usuarios
+    def changeNickname(self, name):
+        self.nickname = name
 
 #envia mensagens para todos, menos para o servidor e para o remetente
-def broadcast(s, msg):
-    global connections
+def broadcast(s,serverSocket,connections, msg):
     for socket in connections:
         if socket != serverSocket and socket != s:
             try:
@@ -11,42 +20,68 @@ def broadcast(s, msg):
                 socket.close()
                 connections.remove(socket)
 
+#detecta o usuario a partir do seu socket
+def detectUser(s,connections,users):
+    for user in users:
+        for socket in connections:
+            if s == user.socket:
+                return user
+            else:
+                continue
 
 def main():
     # definicao das variaveis
     connections = [] #lista para armazenar conexoes, incluindo o servidor
+    users = [] #lista para armazenar usuarios
+
     recvBuffer = 4096
-    serverName = '' # ip do servidor (em branco)
-    serverPort = 12000 # porta a se conectar
+    serverName = '0.0.0.0' # ip do servidor (em branco)
+    serverPort = 1200 # porta a se conectar
     serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # criacao do socket TCP
     serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
-    serverSocket.listen(1) # socket pronto para "ouvir" conexoes
+    serverSocket.listen(10) # socket pronto para "ouvir" conexoes
+
+    connections.append(serverSocket)
 
     print("Chat iniciado na porta {}".format(serverPort))
-   
-    while 1:
+
+    while (1):
         #recebe a lista de sockets prontos para leitura
         readSockets, writeSockets,errorSockets = select.select(connections,[],[])
         for s in readSockets:
-            #nova conexão
+            #nova conexao
             if s == serverSocket:
-                #conexão recebida atraves do serverSocket
+                #conexao recebida atraves do serverSocket
                 connectionSocket, addr = serverSocket.accept()
-                print("Cliente ({},{})".format(addr))
-                broadcast(connectionSocket, "[{}:{} entrou na sala]".format(addr))
+
+                u = User(connectionSocket)
+                connectionSocket.send("Por favor, escolha um nickname \n")
+                data = connectionSocket.recv(recvBuffer)
+
+                while(not data):
+                    connectionSocket.send("Nickname invalido. Por favor,escolha outro\n")
+                    data = connectionSocket.recv(recvBuffer)
+
+                u.changeNickname(data.rstrip('\n'))
+                users.append(u)
+                connections.append(connectionSocket)
+                print("Cliente ({},{}), Nick {}".format(addr[0], addr[1], u.nickname))
+                broadcast(connectionSocket,serverSocket,connections, "[{} entrou na sala]\n".format(u.nickname))
             #mensagem vindo de algum cliente
             else:
+                u = detectUser(s,connections,users)
                 try:
-                    data = s.recv(4096)
+                    data = s.recv(recvBuffer)
                     if data:
-                        broadcast(s, "\r" + '<' + str(s.getpeername()) + '> ' + data)
+                        broadcast(s,serverSocket,connections,'<{}> {}'.format(u.nickname, data))
                 except:
-                    broadcast(s, "Cliente ({}:{}) esta offline".format(addr))
-                    print("Cliente ({}:{}) esta offline".format(addr))
+                    broadcast(s,serverSocket,connections,"{} esta offline".format(u.nickname))
+                    print("Cliente ({}:{}) esta offline".format(addr[0], addr[1]))
                     s.close()
                     connections.remove(s)
+                    users.remove(u)
                     continue
     serverSocket.close()
 
-if __name__ == '_main_':
+if __name__ == "__main__":
     main()
