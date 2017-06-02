129ccf582c69d19106fd5cc6fa74e7e69cdda59b
Igor Macedo <igormacedo94@yahoo.com.br>
2017-06-01 21:49:14 -0300

    implementação de threads

--- Atividade 2/clientTCP.py
+++ Atividade 2/clientTCP.py
@@ -1,8 +1,25 @@
-import socket, select, string, sys
+import socket
+import string
+import sys
+import threading as t
+import time
 
-def prompt() :
-    sys.stdout.write('<You> ')
-    sys.stdout.flush()
+kill = False
+
+def receiveData(conn):
+    global kill
+    while not kill:
+        msg = conn.recv(4096)
+        if msg == "kill":
+            kill = True
+            break
+        print msg
+
+def sendData(conn):
+    global kill
+    while not kill:
+        msg = raw_input()
+        conn.send(msg)
 
 #funcao principal
 if(len(sys.argv) < 3) :
@@ -13,7 +30,6 @@ host = sys.argv[1]
 port = int(sys.argv[2])
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(2)
 
 #Conectar a um servidor
 try :
@@ -24,25 +40,28 @@ except :
 
 print ('Conectado ao servidor.')
 
-while (1):
-    socketList = [sys.stdin, s]
 
-    #recebe a lista de sockets prontos para leitura
-    readSockets, writeSockets,errorSockets = select.select(socketList , [], [])
+t.Thread(target=receiveData,
+        name="receiveData",
+        args=(s,)).start()
 
-    for sock in readSockets:
-        #mensagem vinda do servidor
-        if sock == s:
-            data = sock.recv(4096)
-            if not data :
-                print ('\nDesconectado do servidor')
-                sys.exit()
-            else:
-                #print data
-                sys.stdout.write(data)
+t.Thread(target=sendData,
+        name="sendData",
+        args=(s,)).start()
 
+try:
+    while not kill:
+        time.sleep(1)
 
-        #mensagem enviada pelo cliente
-        else :
-            msg = sys.stdin.readline()
-            s.send(msg)
+    print("Aplicacao terminada. Pressione [enter]")
+
+except KeyboardInterrupt:
+    kill = True
+    print "Disconnected from server"
+    print "Ctrl-c pressed in main..."
+    sys.exit(1)
+
+    #mensagem enviada pelo cliente
+    #else :
+        #msg = sys.stdin.readline()
+        #s.send(msg)
--- Atividade 2/serverTCP.py
+++ Atividade 2/serverTCP.py
@@ -1,4 +1,14 @@
-import socket, select, datetime
+import sys
+import socket
+import select
+import datetime
+import threading as t
+
+
+#variaveis globais
+CLIENTS = {}
+USERS = {}
+recvBuffer = 4096
 
 #classe para o usuario
 class User:
@@ -11,14 +21,15 @@ class User:
         self.nickname = name
 
 #envia mensagens para todos, menos para o servidor e para o remetente
-def broadcast(s,serverSocket,connections, msg):
-    for socket in connections:
-        if socket != serverSocket and socket != s:
+def broadcast(s, msg):
+    print msg ## print message in server
+    for socket in CLIENTS.values():
+        if socket != s:
             try:
                 socket.send(msg)
             except:
                 socket.close()
-                connections.remove(socket)
+                CLIENTS.pop(socket.fileno())
 
 #detecta o usuario a partir de uma porta
 def detectUser(s,users):
@@ -32,7 +43,7 @@ def detectUser(s,users):
     return u
 
 #gerencia os floods
-def flood(socket,date, floodList, dateList):
+def flood(socket, date, floodList, dateList):
 
         sec = datetime.timedelta(seconds=2)
         floodList.append(socket)
@@ -54,96 +65,87 @@ def flood(socket,date, floodList, dateList):
         else:
             return False
 
+def clientConnection(clientSocket, user):
+
+    id = str(user.ip)+":"+str(user.port)
+
+    print "Thread do cliente " + str(user.ip) + ":" +str(user.port) + " iniciou"
+    clientSocket.send("Por favor, escolha um nickname: ")
+    data = clientSocket.recv(recvBuffer)
+
+    while(data == ""):
+        clientSocket.send("Nickname invalido. Por favor,escolha outro\n")
+        data = clientSocket.recv(recvBuffer)
+
+    user.changeNickname(data.rstrip('\n'))
+
+    print("Cliente ({},{}), Nick {} entrou na sala".format(user.ip, user.port, user.nickname))
+
+    clientSocket.send("Comandos do chat: \n")
+    clientSocket.send("/listar - lista todos os usuarios\n")
+    clientSocket.send("/trocarnick - muda o nickname\n")
+    clientSocket.send("/sair - encerra o chat\n")
+
+    broadcast(clientSocket, "[{} entrou na sala]\n".format(user.nickname))
+
+    while True:
+        try:
+            msg = clientSocket.recv(recvBuffer)
+            if msg:
+                if msg.rstrip('\n') == '/listar':
+                    clientSocket.send("Usuarios online: \n")
+                    for u in USERS.values():
+                        clientSocket.send("<{},{},{}>\n".format(u.nickname, u.ip, u.port))
+
+                elif msg.rstrip(msg[12:]) == '/trocarnick ':
+                     newNick = msg[12:].rstrip('\n')
+                     oldNick = user.nickname
+                     u.changeNickname(newNick)
+                     clientSocket.send("Voce mudou de {} para {}\n".format(oldNick, newNick))
+                     broadcast(clientSocket, "{} mudou o nick para {}\n".format(oldNick, newNick))
+                     print("Cliente ({},{}), Nick {}".format(user.ip, user.port, user.nickname))
+
+                elif msg.rstrip('\n') == '/sair':
+                    broadcast(clientSocket,"{} saiu do chat\n".format(user.nickname))
+                    print("Cliente ({}:{}) esta offline".format(user.ip, user.port))
+                    clientSocket.send("kill")
+                    clientSocket.close()
+                    CLIENTS.pop(id)
+                    USERS.pop(id)
+                    break
+                else:
+                    broadcast(clientSocket,'<{}> {}'.format(user.nickname, msg))
+        except :
+            broadcast(clientSocket,"{} esta offline\n".format(user.nickname))
+            print("Cliente ({}:{}) esta offline".format(user.ip, user.port))
+            clientSocket.close()
+            CLIENTS.pop(id)
+            USERS.pop(id)
+
 #codigo principal
 def main():
-    floodList = []
-    dateList = []
+    port = int(sys.argv[1])
 
-    # definicao das variaveis
-    connections = [] #lista para armazenar conexoes, incluindo o servidor
-    users = [] #lista para armazenar usuarios
-
-    recvBuffer = 4096
     serverName = '0.0.0.0' # ip do servidor
-    serverPort = 12000 # porta a se conectar
+    serverPort = port # porta a se conectar
     serverSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # criacao do socket TCP
     serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
     serverSocket.listen(100) # socket pronto para "ouvir" conexoes
 
-    connections.append(serverSocket)
-
     print("Chat iniciado na porta {}".format(serverPort))
-    #print("Comandos")
-    #print("/sair - encerra o chat")
-
 
     while (1):
-        #recebe a lista de sockets prontos para leitura
-        readSockets, writeSockets,errorSockets = select.select(connections,[],[])
-        for s in readSockets:
-            #nova conexao
-            if s == serverSocket:
-                #conexao recebida atraves do serverSocket
-                connectionSocket, addr = serverSocket.accept()
 
-                u = User(addr)
-                connectionSocket.send("Por favor, escolha um nickname \n")
-                data = connectionSocket.recv(recvBuffer)
+        clientSocket, addr = serverSocket.accept()
+        user = User(addr)
 
-                while(not data):
-                    connectionSocket.send("Nickname invalido. Por favor,escolha outro\n")
-                    data = connectionSocket.recv(recvBuffer)
+        id = str(user.ip)+":"+str(user.port)
+        CLIENTS[id] = clientSocket
+        USERS[id] = user
 
-                u.changeNickname(data.rstrip('\n'))
-                users.append(u)
-                connections.append(connectionSocket)
-                print("Cliente ({},{}), Nick {}".format(addr[0], addr[1], u.nickname))
-                connectionSocket.send("Comandos do chat: \n")
-                connectionSocket.send("/listar - lista todos os usuarios\n")
-                connectionSocket.send("/trocarnick - muda o nickname\n")
-                connectionSocket.send("/sair - encerra o chat\n")
-                broadcast(connectionSocket,serverSocket,connections, "[{} entrou na sala]\n".format(u.nickname))
-            #mensagem vindo de algum cliente
-            else:
-                data = datetime.datetime.now()
-                if flood(s, data, floodList, dateList):
-                    s.send("Alerta de flood, espere um pouco\n")
-                    #acho que da pra melhorar isso com thread.sleep()                    
-                else:
-                    u = detectUser(s.getpeername()[1],users)
-                    try:
-                        data = s.recv(recvBuffer)
-                        if data:
-                            if data.rstrip('\n') == '/listar':
-                                s.send("Usuarios online: \n")
-                                for i in range(0,len(users)):
-                                    s.send("<{},{},{}>\n".format(users[i].nickname, users[i].ip, users[i].port))
-
-                            elif data.rstrip(data[11:]) == '/trocarnick':
-                                 newNick = data[11:].rstrip('\n')
-                                 oldNick = u.nickname
-                                 u.changeNickname(newNick)
-                                 s.send("Voce mudou de {} para {}\n".format(oldNick, newNick))
-                                 broadcast(s,serverSocket, connections, "{} mudou o nick para {}\n".format(oldNick, newNick))
-                                 print("{} mudou o nick para {}".format(oldNick, newNick))
-                                 print("Cliente ({},{}), Nick {}".format(addr[0], addr[1], u.nickname))
-
-                            elif data.rstrip('\n') == '/sair':
-                                broadcast(s,serverSocket,connections,"{} saiu do chat\n".format(u.nickname))
-                                print("Cliente ({}:{}) esta offline".format(u.ip, u.port))
-                                s.close()
-                                connections.remove(s)
-                                users.remove(u)
-                            else:
-                                broadcast(s,serverSocket,connections,'<{}> {}'.format(u.nickname, data))
-                    except:
-                        broadcast(s,serverSocket,connections,"{} esta offline\n".format(u.nickname))
-                        print("Cliente ({}:{}) esta offline".format(addr[0], addr[1]))
-                        s.close()
-                        connections.remove(s)
-                        users.remove(u)
-                        continue
-    serverSocket.close()
+        t.Thread(target=clientConnection,
+                name="client"+str(addr[0])+":"+str(addr[1]),
+                args=(clientSocket, user)).start()
 
 if __name__ == "__main__":
     main()
